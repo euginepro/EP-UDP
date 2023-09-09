1fc5d5b131d6345d2c6136cb4068528744d0ed43
euginepro <87873293+euginepro@users.noreply.github.com>
2023-09-10 01:31:45 +0300

    Update old.sh

--- old.sh
+++ old.sh
@@ -808,7 +808,7 @@ check_update() {
 			fi
 			
 			echo -ne "Checking for latest version ... "
-			local _latest_version="$(get_latest_version)"
+			local _latest_version="v1.3.4"
 			if [[ -n "$_latest_version" ]]; then
 				echo "$_latest_version"
 				VERSION="$_latest_version"
